a7705f4e16bb5a121be692103e5d5138868631d4
Mohamed Hassanin <mhassanin@microsoft.com>
2022-09-19 08:58:59 -0400

    init

---
+++ deploy/terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  build_id = "1234"
+}
+
+data "azurerm_client_config" "current" {}
+
+data "azuread_service_principal" "az_sp_pl" {
+  application_id = data.azurerm_client_config.current.client_id
+}
+
+# Resource group for persistent e2e resources
+resource "azurerm_resource_group" "main_rg" {
+  name     = "terraform_rg_123"
+  location = "eastus"
+}
