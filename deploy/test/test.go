d7b5f3ac65ffd39d201a09972eb5e0335c8200e3
Neyissa Exilus <nexilus@microsoft.com>
2022-09-28 18:41:14 -0400

    testing

---
+++ deploy/test/test.go
@@ -0,0 +1,14 @@
+package terraform 
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/azure"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_EndToEnd(t *testing.T) {
+	t.Parallel()
+	assert.True(1,1)
+}
